993e89d60b406b5907990121aed138bdd7c3d198
Alaimo <julien.alaimo@gmail.com>
2019-11-17 12:37:37 +0100

    fix main error

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import Cards.Card;
 import Exception.RentException;
 import KAL2000.DvD;
+import KAL2000.Film;
 import KAL2000.Kal2000;
 import KAL2000.Rent;
+import Util.Category;
+import Util.Human;
+import Util.State;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws RentException, InterruptedException, IOException, ClassNotFoundException {
-        DvD dvD = new DvD();
+        DvD dvD = new DvD(new Film(1,"",new ArrayList<Human>(),new Human("",""), Category.Action), State.Broken,1);
         Rent rent = new Rent(dvD);
         Thread.sleep(400);
         rent.setDateReturn();
